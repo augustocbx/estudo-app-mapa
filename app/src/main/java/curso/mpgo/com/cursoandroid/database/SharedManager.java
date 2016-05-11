25fd21a3215f01624e15fa35c88a6a4086c50374
augustocbx <augustocbx@gmail.com>
2016-05-11 14:14:13 -0300

    New version

---
+++ app/src/main/java/curso/mpgo/com/cursoandroid/database/SharedManager.java
@@ -0,0 +1,9 @@
+package curso.mpgo.com.cursoandroid.database;
+
+/**
+ * Created by augustocbx on 5/11/16.
+ */
+public class SharedManager {
+    private static final String DEFAULT_CONFIG = "default_config";
+    private static final String GOOGLE_SIGNIN = "google_signin";
+}
